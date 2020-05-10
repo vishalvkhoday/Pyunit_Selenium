5e530119dbff3534d28b364b827c1f450d6899b5
vishalvkhoday <vishalvkhoday@gmail.com>
2020-05-10 18:01:11 +0530

    cdi changes as on 10 may 2020

--- Pyunit_Selenium/sel_test.py
+++ Pyunit_Selenium/sel_test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from pytest import fixtures
+from allure import *
 
 
 driver = webdriver.Chrome(executable_path='WebDriver/chromedriver_235')
 
 driver.get('https://www.bseindia.com')
+# driver.find_element_by_xpath("//*[@id=='cls']")
