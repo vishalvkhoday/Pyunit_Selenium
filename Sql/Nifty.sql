f233b9d4ca460df438338be79240474c0b25b1d8
vishalvkhoday <vishalvkhoday@gmail.com>
2020-07-14 20:32:29 +0530

    Nifty ragne1,2

--- Sql/Nifty.sql
+++ Sql/Nifty.sql
@@ -1,6 +1,13 @@
 
+
+
 select scr,round(avg(Hi),2) Hi,round(Avg(lw),2) Lw,round((avg(Hi) + Avg(lw))/2,2) Avg_HnL,round(sqrt(avg(Hi) * Avg(lw)),2)Sqrt_HnL,
-round(((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)),2)/2 pvtpoint,ne.[Close]Cls,ne.Trnx_date from (
+round(((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)),2)/2 pvtpoint,
+round(((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2) +((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2)*0.033,2) pvt_UpRange,
+round(((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2) +((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2)*0.066,2) pvt_UpRange1,
+round(((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2) +((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2)*0.099,2) pvt_UpRange2,
+round(((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2) -((((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)))/2)*0.033,2) pvt_LowRange,
+ne.[Close]Cls,ne.Trnx_date from (
 select Script scr,max([High])Hi,Min([Low])Lw from Nifty where Trnx_date > = (
 select top 1 trnx from (
 select distinct top 120  Trnx_date  trnx from Nifty
@@ -34,9 +41,6 @@ order by Trnx_date desc
 
 ) as T2  inner join Nifty ne on 
 ne.Script =T2.scr
---and ne.Trnx_date = (select max(Trnx_date) from Nifty)
+and ne.Trnx_date = (select max(Trnx_date) from Nifty)
 group by scr,ne.[Close],ne.Trnx_date
-having ne.[Close] between (round(((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)),2)/2) -(round(((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)),2)/2)*0.075
-and (round(((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)),2)/2) +(round(((avg(Hi) + Avg(lw))/2) + sqrt(avg(Hi) * Avg(lw)),2)/2)*0.075
---having ne.[Close] >=round((avg(Hi) + Avg(lw))/2,2) 
 order by 8 desc
